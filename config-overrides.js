a77028f53dd6f01b5c5ecfd17b67e929c2be196a
shittyfibby <118930461+shittyfibby@users.noreply.github.com>
2022-12-08 16:49:00 -0800

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -19,9 +19,3 @@ module.exports = function override(config) {
   return config;
 };
 
-const candyShop = await CandyShop.initSolCandyShop({
-  candyShopCreatorAddress: "DeXFanauSzkQPriAW52N2eq2PBo4TgTsjk5Tx33t4KX9", // Candy Shop owner address
-  treasuryMint: "FrHe1NUX2hRgnpZCTcxDEh8LU9uYEGU1uWhyYjvXLdFe", // Candy Shop transaction currency
-  candyShopProgramId: "csbMUULiQfGjT8ezT16EoEBaiarS6VWRevTw1JMydrS", // Candy Shop program ID
-  env: "mainnet-beta", // network
-});
